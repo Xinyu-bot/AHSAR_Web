c2c23cbfc56e9f3e0c6330ce1bb1a5ea3abb203a
lh2510 <lh2510@nyu.edu>
2021-08-22 21:54:09 -0400

    hsearch history finished(array of object)

--- frontend/src/home/header/searched_list/SearchedList.js
+++ frontend/src/home/header/searched_list/SearchedList.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import SearchedItem from './searched_item/SearchedItem'
+import './SearchedList.css'
 
 export default function SearchedList(props) {
+	console.log('22', props.searchedList)
 	return (
-		<ul>
-			
+		<ul className='searchedList'>
+			{props.searchedList.map((item) => (
+				<SearchedItem key={item.id} name={item.name} pid={item.pid} />
+			))}
 		</ul>
 	)
 }
