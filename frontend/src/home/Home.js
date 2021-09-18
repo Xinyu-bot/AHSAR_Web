3292eb704ef83018714f76032fb6ac550c7051ac
lh2510 <lh2510@nyu.edu>
2021-09-18 12:05:05 -0400

    footer

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -5,15 +5,18 @@ import Header from './header/Header'
 import Result from './result/Result'
 import Article from './result/Article'
 import Content from './content/Content'
-
+import './Home.css'
 function Home() {
 	return (
-		<div className='home'>
+		<div className='Home'>
 			<Switch>
 				<Route path='/result' component={Result}></Route>
 				<Route path='/search_by_name' component={Search}></Route>
 				<Route path='/' component={Content}></Route>
 			</Switch>
+			<div id='footer'>
+				粤ICP备2021131165号-1 | 粤ICP备2021131165号 | <a href='https://beian.miit.gov.cn/#/Integrated/recordQuery'>工信部查询</a>{' '}
+			</div>
 		</div>
 	)
 }
